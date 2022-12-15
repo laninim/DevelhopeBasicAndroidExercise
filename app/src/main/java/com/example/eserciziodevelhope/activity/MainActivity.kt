8b6b888ce25ca18b18ab0dfccd54573a43b3c5d3
Mauro Lanini <mallettomachico@gmail.com>
2022-12-15 19:03:15 +0100

    Add navigation menu e graph navigation system

--- app/src/main/java/com/example/eserciziodevelhope/activity/MainActivity.kt
+++ app/src/main/java/com/example/eserciziodevelhope/activity/MainActivity.kt
@@ -2,11 +2,39 @@ package com.example.eserciziodevelhope.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
 import com.example.eserciziodevelhope.R
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater : MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.optionmenusettings -> {
+                Toast.makeText(applicationContext, "Settings", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.optionmenucontacts -> {
+                Toast.makeText(applicationContext, "Contacts", Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+
 }
